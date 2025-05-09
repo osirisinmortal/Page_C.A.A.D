468229420cdeca0fabc427d607f223208c9c18d5
Sebastian <desarrollo@bstadium.es>
2025-05-09 17:34:14 -0500

    se adiciono comentario

--- src/app/data/page.js
+++ src/app/data/page.js
@@ -5,6 +5,8 @@ import { Container } from 'react-bootstrap';
 
 const App = () => {
 
+    console.log("Esta es la pagina de data");
+
     return (
         <Container>
             Esta es la pagina de data
